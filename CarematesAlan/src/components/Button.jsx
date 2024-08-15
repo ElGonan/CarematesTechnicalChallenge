f81ff7bbb940e0010da5330c0678109c0bddcfe6
XxGonanxX <alanpagzz@outlook.com>
2024-08-14 20:18:54 -0600

    chore: hover added

--- CarematesAlan/src/components/Button.jsx
+++ CarematesAlan/src/components/Button.jsx
@@ -3,7 +3,7 @@
  */
 // eslint-disable-next-line react/prop-types
 const Button = ({ children }) => (
-    <button className=' bg-green-300 rounded-lg h-fit w-fit px-4 py-4 text-white'>
+    <button className=' bg-green-300 rounded-lg h-fit w-fit px-4 py-4 text-white hover:bg-green-700'>
       { children }
     </button>
   );
